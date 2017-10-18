190ee3795502dae2d1212c1c8b2427dde25b03e7
Randle McMurphy <jmarton2000@gmail.com>
2017-10-18 19:55:35 +0300

    Added 13 DOM Manipulation

---
+++ ColtSteeleWebDev/13 DOM Manipulation/myLoop.js
@@ -0,0 +1,7 @@
+var imgs = document.querySelectorAll("img")
+
+for (var i = 0; i < imgs.length; i++) {
+	
+    imgs[i].setAttribute("src", "http://www.readersdigest.ca/wp-content/uploads/2011/01/4-ways-cheer-up-depressed-cat.jpg")
+	
+}
